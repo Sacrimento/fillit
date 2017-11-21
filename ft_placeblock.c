11af2e9f184db1fc772fb96e8299c983406e61e7
Razshal <fontenille.mathieu@gmail.com>
2017-11-21 12:06:26 +0100

    block placer needs tests

--- ft_placeblock.c
+++ ft_placeblock.c
@@ -6,15 +6,25 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/19 13:53:57 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/11/19 16:38:29 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/11/21 12:03:49 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./fillit.h"
 
+int	ft_placeblock(char *block, char **grid, int cline, int crow, char c);
+
 static void ft_revlast(char *block, char **grid, int cline, int crow, char c)
 {
-	
+	if (block[-2] == 'l')
+		cline++;
+	if (block[-2] == 'r')
+		cline--;
+	if (block[-2] == 'u')
+		grid++;
+	if (block[-2] == 'd')
+		grid--;
+	ft_placeblock(block++, grid, cline, crow, c);
 }
 
 /* Tests if the current placement is ok for this block */
@@ -40,9 +50,13 @@ int	ft_placeblock(char *block, char **grid, int cline, int crow, char c)
 			placeres = ft_placeblock(block++, grid + 1, cline, crow, c);
 		if (block[0] == 'b')
 		{
-			ft_revlast(block, grid, cline, crow, c);
+			ft_revlast(block++, grid, cline, crow, c);
 		}
 	}
-		grid[crow][cline] = c;
-		return (1);
+		if (placeres == 1)
+		{
+			grid[crow][cline] = c;
+			return (placeres);
+		}
+		return (0);
 }
