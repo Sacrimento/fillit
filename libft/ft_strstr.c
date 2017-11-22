d014957b2fc533941b541201b4bba0f8c0e1e878
Augustin BOUVEROT <abouvero@e2r4p5.42.fr>
2017-11-22 15:46:47 +0100

    fixed lib issues

--- libft/ft_strstr.c
+++ libft/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: abouvero <abouvero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 14:59:29 by abouvero          #+#    #+#             */
-/*   Updated: 2017/11/22 15:34:28 by abouvero         ###   ########.fr       */
+/*   Updated: 2017/11/22 15:44:42 by abouvero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ int		ft_strstr(const char *haystack, const char *needle)
 {
 	int			i;
 	int			j;
-	const char	*ptr;
 
 	i = 0;
 	j = 0;
 	if (needle[0] == '\0')
-		return ((char*)haystack);
+		return (0);
 	while (haystack[i])
 	{
 		if (haystack[i] == needle[0])
