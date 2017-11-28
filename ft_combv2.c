fad647307c5c98d2ac4379ad30fecc9911824395
Razshal <fontenille.mathieu@gmail.com>
2017-11-28 20:19:12 +0100

    combv2 wip

--- ft_combv2.c
+++ ft_combv2.c
@@ -6,31 +6,49 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 18:34:15 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/11/28 19:55:06 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/11/28 20:18:45 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-static int endoftab(struct	*);
+static int is_blocks_left(struct *s_block_sort);
 static int already_set(struct s_block_sort);
 static void mark_as_set(struct s_block_sort);
+static void mark_as_notset(struct s_block_sort);
 
-//maintab is a struct array with the blocks in alphabetic order
-//curentab was a fresh tab large as maintab, quite useless : I try to do the job without it
+/* ft_combv2 a combinasion generator with recursive backtracking
+ * ///////////////=================================================
+ * maintab is a struct array with the blocks in alphabetic order
+ * curentab was a fresh tab large as maintab where we stored the current combinasion
+ * trough the recursion, quite useless : I try to do the job without
+ * it but it use to help me visualise the current action
+ * ///////////////=================================================
+ */
 
-int combv2(struct t_block_sort *maintab, int start)
+int combv2(struct s_block_sort *maintab, int start)
 {
-	if (!endoftab(curentab[0]))
+	int local_start;
+
+	if (end_of_tab(maintab[start]))
+		return (0);
+	if (!is_blocks_left(maintab))
 		return (1);
 	while (!already_set(maintab[start]))
 		start++;
 //if we can't place this block, we are retring the current recursion with another start
 	if (!ft_placenext(maintab[start]))
-		combv2(maintab, curentab, start);
+		combv2(maintab, start + 1);
 	else
 		mark_as_set(maintab[start]);
+	local_start = start;
 	start = 0;
 	if (combv2(maintab, start))
-		
+		return (1);
+	else
+	{
+		mark_as_notset(maintab[local_start]);
+		combv2(maintab, local_start + 1);
+	}
+	return (0);
 }
