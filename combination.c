c1209fe993b9091d640bf5ceb5725d6662753092
Razshal <fontenille.mathieu@gmail.com>
2017-12-06 15:02:48 +0100

    lightfix on combination.c

--- combination.c
+++ combination.c
@@ -6,13 +6,13 @@
 /*   By: mfonteni <mfonteni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 18:34:15 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/12/06 14:21:10 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/12/06 15:02:00 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int	combination(t_block_sort *maintab, int start, int decal, char **grid)
+int	combination(t_block_sort *maintab, int start, char **grid)
 {
 	if (!is_blocks_left(maintab))
 		return (1);
@@ -22,17 +22,17 @@ int	combination(t_block_sort *maintab, int start, int decal, char **grid)
 		return (0);
 	if (already_set(maintab[start]))
 		return (0);
-	if (!placenext((maintab[start]).block, grid, start + 'A', decal))
-		return (combination(maintab, start + 1, 0, grid));
+	if (!placenext((maintab[start]).block, grid, start + 'A'))
+		return (combination(maintab, start + 1, grid));
 	else
 		maintab[start] = mark_as_set(maintab[start]);
-	if (combination(maintab, 0, 0, grid))
+	if (combination(maintab, 0, grid))
 		return (1);
 	else
 	{
 		maintab[start] = mark_as_notset(maintab[start]);
 		eraseblock(grid, start + 'A');
-		return (combination(maintab, start + 1, 0, grid));
+		return (combination(maintab, start + 1, grid));
 	}
 	return (0);
 }
